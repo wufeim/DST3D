c55444858316dd98dc22f415b30cc56795adaab3
Wufei Ma <wufeim@gmail.com>
2023-11-30 14:51:05 -0500

    update dst3d.utils for training

--- dst3d/utils/general.py
+++ dst3d/utils/general.py
@@ -1,4 +1,10 @@
+from datetime import datetime
+import logging
 import os
+import random
+
+import numpy as np
+import torch
 
 
 def get_pkg_root():
@@ -17,3 +23,48 @@ def get_abs_path(path):
     if not os.path.isabs(path):
         path = os.path.join(get_project_root(), path)
     return path
+
+
+def str2bool (val):
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError(f"invalid truth value {val}")
+
+
+def setup_logging(save_path):
+    os.makedirs(os.path.join(save_path, "ckpts"), exist_ok=True)
+    dt = datetime.now().strftime("%Y%m%d_%H%M%S")
+    logging.root.handlers = []
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        filename=os.path.join(save_path, f"log_{dt}.txt"),
+        filemode="w",
+    )
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    console.setFormatter(formatter)
+    logging.getLogger("").addHandler(console)
+    return logging.getLogger("").handlers[0].baseFilename
+
+
+def set_seed(seed):
+    if seed is not None:
+        random.seed(seed)
+        torch.manual_seed(seed)
+        try:
+            import transformers
+
+            transformers.set_seed(seed)
+        except ImportError:
+            pass
+        logging.info(f"Set random seed to {seed}")
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
