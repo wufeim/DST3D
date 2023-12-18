2cc4268b76add48120c4dd0550f6d914a10a7de1
Wufei Ma <wufeim@gmail.com>
2023-12-17 22:30:45 -0500

    add dnnlib.EasyDict

--- dst3d/utils/__init__.py
+++ dst3d/utils/__init__.py
@@ -1,3 +1,4 @@
+from .dnnlib import EasyDict
 from .general import setup_logging, set_seed, str2bool
 from .objaverse import get_objaverse_path_from_uid, get_objaverse_url_from_uid
 from .pose_error import pose_error
