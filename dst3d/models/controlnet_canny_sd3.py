5f98898a3a27b23a6396c9ea8b554c746eb501bd
Sky Cen <83487980+censky2@users.noreply.github.com>
2025-03-30 22:07:29 -0400

    Added SD3 ControlNet

---
+++ dst3d/models/controlnet_canny_sd3.py
@@ -0,0 +1,48 @@
+import cv2
+from diffusers import StableDiffusion3ControlNetPipeline
+from diffusers.models import SD3ControlNetModel
+import numpy as np
+from PIL import Image
+import torch
+
+from .base_model import BaseModel
+
+
+class ControlNetCannySD3(BaseModel):
+    def __init__(self, model_name='InstantX/SD3-Controlnet-Canny', canny_lower=100, canny_upper=200, controlnet_conditioning_scale=1.0, device='cuda', **kwargs):
+        super().__init__(model_name, device)
+        self.canny_lower = canny_lower
+        self.canny_upper = canny_upper
+        self.controlnet_conditioning_scale = controlnet_conditioning_scale
+
+        # Build ControlNet pipeline
+        controlnet = SD3ControlNetModel.from_pretrained(
+            "InstantX/SD3-Controlnet-Canny",
+            torch_dtype=torch.float16).to(device)
+        self.pipe = StableDiffusion3ControlNetPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-3-medium-diffusers", 
+            controlnet=controlnet,
+            torch_dtype=torch.float16).to(device)
+        self.pipe.enable_model_cpu_offload()
+
+
+    def get_condition(self, image):
+        edges = cv2.Canny(image, self.canny_lower, self.canny_upper)
+        edges = edges[:, :, np.newaxis]
+        edges = np.concatenate([edges, edges, edges], axis=2)
+        return Image.fromarray(edges)
+
+
+    @torch.no_grad()
+    def forward(self, image=None, visual_prompt=None, prompt=None, negative_prompt=None, strength=None):
+        assert prompt is not None and negative_prompt is not None
+        assert image is not None or visual_prompt is not None
+
+        if strength is None:
+            strength = self.controlnet_conditioning_scale
+
+        print(len(image))
+        if visual_prompt is None:
+            visual_prompt = [self.get_condition(im) for im in image]
+
+        return self.pipe(prompt, negative_prompt=negative_prompt, control_image=visual_prompt, controlnet_conditioning_scale=strength).images
---
+++ dst3d/models/multicontrolnet_canny_depth_sdxl.py
@@ -0,0 +1,95 @@
+import cv2
+from diffusers import StableDiffusionXLControlNetPipeline, ControlNetModel, AutoencoderKL, UniPCMultistepScheduler
+import numpy as np
+from PIL import Image
+import torch
+from transformers import DPTImageProcessor, DPTForDepthEstimation
+
+from .base_model import BaseModel
+
+# Must use batch_size == 1
+class MultiControlNetCannyDepthSDXL(BaseModel):
+    def __init__(self, model_name='multicontrolnet-canny-depth-sdxl-1.0', canny_lower=100, canny_upper=200, controlnet_conditioning_scale=1.0, device='cpu', **kwargs):
+        super().__init__(model_name, device)
+        self.canny_lower = canny_lower
+        self.canny_upper = canny_upper
+        self.controlnet_conditioning_scale = controlnet_conditioning_scale
+        self.device = device
+        
+        # Build Midas pipeline
+        self.midas_model = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas", low_cpu_mem_usage=True)
+        self.midas_image_processor = DPTImageProcessor.from_pretrained("Intel/dpt-hybrid-midas")
+        
+        # Build ControlNet pipeline
+        controlnets = [
+            ControlNetModel.from_pretrained(
+                "diffusers/controlnet-canny-sdxl-1.0", variant="fp16", torch_dtype=torch.float16, use_safetensors=True
+            ),
+            ControlNetModel.from_pretrained(
+                "diffusers/controlnet-depth-sdxl-1.0", variant="fp16", torch_dtype=torch.float16, use_safetensors=True
+            ),
+        ]
+        vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16, use_safetensors=True)
+        self.pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-xl-base-1.0", controlnet=controlnets, variant="fp16", vae=vae, torch_dtype=torch.float16, use_safetensors=True
+        )
+        self.pipe.scheduler = UniPCMultistepScheduler.from_config(self.pipe.scheduler.config)
+        self.pipe.enable_model_cpu_offload()
+
+
+    def get_condition(self, image):
+        edges = cv2.Canny(image, self.canny_lower, self.canny_upper)
+        edges = edges[:, :, np.newaxis]
+        edges = np.concatenate([edges, edges, edges], axis=2)
+        return Image.fromarray(edges)
+    
+
+    def get_depth(self, image):
+        image = Image.fromarray(image)
+        inputs = self.midas_image_processor(images=image, return_tensors="pt")
+
+        with torch.no_grad():
+            outputs = self.midas_model(**inputs)
+            depth = outputs.predicted_depth
+
+        depth = torch.nn.functional.interpolate(
+            depth.unsqueeze(1),
+            size=image.size[::-1],
+            mode="bicubic",
+            align_corners=False,
+        )
+
+        depth = depth.squeeze().cpu().numpy()
+        depth = (depth * 255 / np.max(depth)).astype("uint8")
+        depth = Image.fromarray(depth).convert('RGB')
+        return depth
+    
+
+    @torch.no_grad()
+    def forward(self, image=None, visual_prompt=None, prompt=None, negative_prompt=None, strength=None):
+        assert prompt is not None and negative_prompt is not None
+        assert image is not None or visual_prompt is not None
+
+        if strength is None:
+            strength = self.controlnet_conditioning_scale
+
+        if visual_prompt is None:
+            canny_conditions = []
+            depth_conditions = []
+            
+            for im in image:
+                canny_condition = self.get_condition(im)
+                canny_conditions.append(canny_condition)
+                
+                depth_condition = self.get_depth(im)
+                depth_conditions.append(depth_condition)
+            
+            if len(image) == 1:
+                visual_prompt = [canny_conditions[0], depth_conditions[0]]
+            else:
+                visual_prompt = [canny_conditions, depth_conditions]
+        
+        if isinstance(strength, (int, float)):
+            strength = [strength, strength]
+
+        return self.pipe(prompt, image=visual_prompt, negative_prompt=negative_prompt, controlnet_conditioning_scale=strength).images
